ebe94c59a620bd26804bede71492bb9b67aa534f
Fabio Giannoccoli <fabio.giannoccoli@gmail.com>
2024-08-21 20:16:12 +0200

    Modified app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def create_app(db_url=None):
     load_dotenv()
     app.config["PROPAGATE EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Movies API"
-    # app.config["API_VERSION"] = "v1"
+    app.config["API_VERSION"] = "v1"
     # app.config["OPENAPI_VERSION"] = "3.0.3"
     # app.config["OPENAPI_URL_PREFIX"] = "/"
     # app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
