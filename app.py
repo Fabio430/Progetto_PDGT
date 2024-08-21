b47db97555c97f419bcdef6c4facaeb3914d9f70
Fabio Giannoccoli <fabio.giannoccoli@gmail.com>
2024-08-21 18:54:18 +0200

    Modified app.py

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ def create_app(db_url=None):
     app = Flask(__name__)
     load_dotenv()
     app.config["PROPAGATE EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Movies API"
+    # app.config["API_VERSION"] = "v1"
+    # app.config["OPENAPI_VERSION"] = "3.0.3"
+    # app.config["OPENAPI_URL_PREFIX"] = "/"
+    # app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    # app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
